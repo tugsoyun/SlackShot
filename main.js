bfa322cef796bb254e48bf2967cb2557f5e89ee2
togsoyun <togsoyun@gmail.com>
2025-04-06 01:41:37 +0000

    Working Image Analyzer

--- main.js
+++ main.js
@@ -15,36 +15,32 @@ async function getWebCam() {
 }
 
 getWebCam(); 
+
 function captureImage() {
   var canvas=document.getElementById("canvas");
   var context=canvas.getContext('2d');
+  var video = document.getElementById("video");
   canvas.width=video.videoWidth;
   canvas.height=video.videoHeight;
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
   var image=canvas.toDataURL('image/jpeg');
+  analyzeImage(image);
 }
 
 // Start the interval
-let intervalId = setInterval(myFunction, 1000);
+setInterval(captureImage, 10000);
 
 // 🔥🔥 FILL THIS OUT FIRST! 🔥🔥
 // Get your Gemini API key by:
 // - Selecting "Add Gemini API" in the "Project IDX" panel in the sidebar
 // - Or by visiting https://g.co/ai/idxGetGeminiKey
 let API_KEY = 'AIzaSyCrjkiCTgZfQp-BYIdPsXC6g6PajzXTYRc';
-
-let form = document.querySelector('form');
-let promptInput = document.querySelector('input[name="prompt"]');
 let output = document.querySelector('.output');
 
-form.onsubmit = async (ev) => {
-  ev.preventDefault();
-  output.textContent = 'Generating...';
-
+async function analyzeImage(imageURL) {
   try {
     // Load the image as a base64 string
-    let imageUrl = form.elements.namedItem('chosen-image').value;
-    let imageBase64 = await fetch(imageUrl)
+    let imageBase64 = await fetch(imageURL)
       .then(r => r.arrayBuffer())
       .then(a => Base64.fromByteArray(new Uint8Array(a)));
 
@@ -54,7 +50,7 @@ form.onsubmit = async (ev) => {
         role: 'user',
         parts: [
           { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
-          { text: promptInput.value }
+          { text: "Is the person in the image on their phone? Enter 0 for no, 1 for yes." }
         ]
       }
     ];
@@ -78,12 +74,66 @@ form.onsubmit = async (ev) => {
     let md = new MarkdownIt();
     for await (let response of result.stream) {
       buffer.push(response.text());
+      console.log(response.text);
       output.innerHTML = md.render(buffer.join(''));
     }
   } catch (e) {
     output.innerHTML += '<hr>' + e;
   }
-};
+}
 
-// You can delete this once you've filled out an API key
-maybeShowApiKeyBanner(API_KEY);
+
+// let form = document.querySelector('form');
+// let promptInput = document.querySelector('input[name="prompt"]');
+// let output = document.querySelector('.output');
+
+// form.onsubmit = async (ev) => {
+//   ev.preventDefault();
+//   output.textContent = 'Generating...';
+
+//   try {
+//     // Load the image as a base64 string
+//     let imageUrl = form.elements.namedItem('chosen-image').value;
+//     let imageBase64 = await fetch(imageUrl)
+//       .then(r => r.arrayBuffer())
+//       .then(a => Base64.fromByteArray(new Uint8Array(a)));
+
+//     // Assemble the prompt by combining the text with the chosen image
+//     let contents = [
+//       {
+//         role: 'user',
+//         parts: [
+//           { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
+//           { text: promptInput.value }
+//         ]
+//       }
+//     ];
+
+//     // Call the multimodal model, and get a stream of results
+//     const genAI = new GoogleGenerativeAI(API_KEY);
+//     const model = genAI.getGenerativeModel({
+//       model: "gemini-1.5-flash", // or gemini-1.5-pro
+//       safetySettings: [
+//         {
+//           category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+//           threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+//         },
+//       ],
+//     });
+
+//     const result = await model.generateContentStream({ contents });
+
+//     // Read from the stream and interpret the output as markdown
+//     let buffer = [];
+//     let md = new MarkdownIt();
+//     for await (let response of result.stream) {
+//       buffer.push(response.text());
+//       output.innerHTML = md.render(buffer.join(''));
+//     }
+//   } catch (e) {
+//     output.innerHTML += '<hr>' + e;
+//   }
+// };
+
+// // You can delete this once you've filled out an API key
+// maybeShowApiKeyBanner(API_KEY);
